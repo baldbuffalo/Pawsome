46de31315fadc8c3937ada703cb44a700fed8587
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2024-06-14 18:40:00 +0400

    Create CatFormActivity.kt

---
+++ app/src/main/java/com/example/pawsome/CatFormActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pawsome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CatFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cat_form_activity) // Assuming your XML file is named cat_form.xml
+    }
+}
